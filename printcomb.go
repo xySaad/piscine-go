3bdab02f3130b9fc66e1a25283a0a952dffd1f82
srm <saadrm.dev@gmail.com>
2024-07-24 18:59:28 +0100

    .

--- printcomb.go
+++ printcomb.go
@@ -3,13 +3,13 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for i := '0'; i <= '9'; i++ {
-		for j := i + 1; j <= '9'; j++ {
-			for k := j + 1; k <= '9'; k++ {
-				z01.PrintRune(i)
-				z01.PrintRune(j)
-				z01.PrintRune(k)
-				if i == '7' && j == '8' && k == '9' {
+	for a := '0'; a < '9'; a++ {
+		for b := a + 1; b < '9'; b++ {
+			for c := b + 1; c < '9'; c++ {
+				z01.PrintRune(a)
+				z01.PrintRune(b)
+				z01.PrintRune(c)
+				if a == '7' && b == '8' && c == '9' {
 					z01.PrintRune('\n')
 				} else {
 					z01.PrintRune(',')
@@ -18,4 +18,5 @@ func PrintComb() {
 			}
 		}
 	}
+	z01.PrintRune('\n')
 }
