3134cf3598a574efa4a99775ad16271118841e4d
srm <saadrm.dev@gmail.com>
2024-07-25 09:51:57 +0100

    printcomb2

---
+++ printcomb2.go
@@ -0,0 +1,34 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	var print bool = true
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				for l := '0'; l <= '9'; l++ {
+					if i < k {
+						print = true
+					} else {
+						if j < l {
+							print = true
+						} else {
+							print = false
+						}
+					}
+
+					if print {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(' ')
+						z01.PrintRune(k)
+						z01.PrintRune(l)
+						z01.PrintRune(',')
+						z01.PrintRune('\n')
+					}
+				}
+			}
+		}
+	}
+}
