f1c405de5c528abdfada6be8a9525f6e8607fe17
srm <saadrm.dev@gmail.com>
2024-07-26 12:56:17 +0100

    .

--- basicatoi2.go
+++ basicatoi2.go
@@ -1,18 +1,17 @@
-package piscine 
+package piscine
 
+func BasicAtoi2(s string) int {
+	var numberAsInteger int = 0
 
-func BasicAtoi2(s string) int{
-    var numberAsInteger int = 0
+	for i := range s {
+		var num int = 0
 
-    for i := range s{
-        var num int = 0
+		if s[i] >= '0' && s[i] < '9' {
+			num = int(s[i] - '0')
+		}
 
-        if s[i] >= '0' && s[i] < '9' {
-            num = int(s[i] - '0')
-        }
-
-        numberAsInteger = numberAsInteger*10 + num
-    }
-return numberAsInteger
+		numberAsInteger = numberAsInteger*10 + num
+	}
+	return numberAsInteger
 
 }
