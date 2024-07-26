a9c8f45a997f1e378ded93b2b4fd5b03b8350a40
srm <saadrm.dev@gmail.com>
2024-07-26 12:53:23 +0100

    basicatoi2

--- printcomb.go
+++ printcomb.go
@@ -6,17 +6,11 @@ import (
 
 func PrintComb() {
 	for i := '0'; i <= '9'; i++ {
-		for j := i + 1; j <= '9'; j++ {
-			for k := j + 1; k <= '9'; k++ {
+		for m := i + 1 ; m <= '9' ; m++ {
+			for a := m + 1 ; a <= '9' ; a++ {
 				z01.PrintRune(i)
-				z01.PrintRune(j)
-				z01.PrintRune(k)
-				if i != '7' || j != '8' || k != '9' {
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
-				}
+
 			}
-		}
+		}	
 	}
-	z01.PrintRune('\n')
 }
