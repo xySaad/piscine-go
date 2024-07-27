25a76e8d787ad86316c018795bb4bd9a2001059b
srm <saadrm.dev@gmail.com>
2024-07-27 16:02:55 +0100

    .

--- basicatoi2.go
+++ basicatoi2.go
@@ -6,6 +6,8 @@ func BasicAtoi2(s string) int {
 		var num int = 0
 		if s[i] >= '0' && s[i] <= '9' {
 			num = int(s[i] - '0')
+		} else {
+			return 0
 		}
 		numberAsInteger = numberAsInteger*10 + num
 	}
