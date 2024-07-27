12150f217981d4021bd8d1f91baca0f46563ef6f
srm <saadrm.dev@gmail.com>
2024-07-27 15:59:06 +0100

    basicatoi2

--- basicatoi2.go
+++ basicatoi2.go
@@ -4,7 +4,7 @@ func BasicAtoi2(s string) int {
 	var numberAsInteger int = 0
 	for i := range s {
 		var num int = 0
-		if s[i] >= '0' && s[i] < '9' {
+		if s[i] >= '0' && s[i] <= '9' {
 			num = int(s[i] - '0')
 		}
 		numberAsInteger = numberAsInteger*10 + num
