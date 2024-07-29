8cad0381a7135405f2c4dbbe3e59847e37b1e4e9
srm <saadrm.dev@gmail.com>
2024-07-29 17:19:56 +0100

    Fix: IsPrime(2) returns false

--- isprime.go
+++ isprime.go
@@ -4,9 +4,13 @@ func IsPrime(nb int) bool {
 	if nb%2 == 0 {
 		return false
 	}
-	if nb <= 2 {
+	if nb == 1 {
 		return false
 	}
+	if nb == 2 {
+		return true
+	}
+
 	for i := 2; i <= nb; i++ {
 		if i != nb {
 			if nb%i == 0 {
