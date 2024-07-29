ddeca29bdf5a2d86ecaf5ea86329d0c10c3a8e63
srm <saadrm.dev@gmail.com>
2024-07-29 17:32:32 +0100

    .

--- isprime.go
+++ isprime.go
@@ -1,14 +1,14 @@
 package piscine
 
 func IsPrime(nb int) bool {
+	if nb == 2 {
+		return true
+	}
 	if nb%2 == 0 {
 		return false
 	}
 	if nb <= 1 {
 		return false
-	}
-	if nb == 2 {
-		return true
 	}
 
 	for i := 2; i <= nb; i++ {
