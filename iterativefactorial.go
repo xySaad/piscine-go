b0c8fdfd341e892d4e0b57ff4a574d9c2706dfb7
srm <saadrm.dev@gmail.com>
2024-07-29 11:36:23 +0100

    .

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,7 +1,5 @@
 package piscine
 
-import "math"
-
 func IterativeFactorial(nb int) int {
 	if nb < 0 {
 		return 0
@@ -9,9 +7,7 @@ func IterativeFactorial(nb int) int {
 	result := 1
 
 	for i := nb; i >= 1; i-- {
-		if result > math.MaxInt/i {
-			return 0
-		}
+
 		result *= i
 	}
 
