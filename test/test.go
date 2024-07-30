0643dfa379b6b75200458e8346c31ad06c662b89
srm <saadrm.dev@gmail.com>
2024-07-30 10:09:49 +0100

    Fix: firstrune

--- test/test.go
+++ test/test.go
@@ -10,5 +10,6 @@ func main() {
 	z01.PrintRune(piscine.FirstRune("Hello!"))
 	z01.PrintRune(piscine.FirstRune("Salut!"))
 	z01.PrintRune(piscine.FirstRune("Ola!"))
+	z01.PrintRune(piscine.FirstRune("♥01"))
 	z01.PrintRune('\n')
 }
