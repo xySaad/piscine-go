fde01d01c97bb1b6df39905a5e3ca47fa95a2db8
srm <saadrm.dev@gmail.com>
2024-07-30 09:22:15 +0100

    firstrune

--- test/test.go
+++ test/test.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"github.com/01-edu/z01"
+
 	"piscine"
 )
 
 func main() {
-	fmt.Println(piscine.FindNextPrime(5))
-	fmt.Println(piscine.FindNextPrime(4))
+	z01.PrintRune(piscine.FirstRune("Hello!"))
+	z01.PrintRune(piscine.FirstRune("Salut!"))
+	z01.PrintRune(piscine.FirstRune("Ola!"))
+	z01.PrintRune('\n')
 }
