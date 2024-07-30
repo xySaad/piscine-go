9cf39c52c465fbd6728946b6ce374aa471cfd41d
srm <saadrm.dev@gmail.com>
2024-07-30 10:27:51 +0100

    Fix: nrune

--- nrune.go
+++ nrune.go
@@ -2,9 +2,9 @@ package piscine
 
 func NRune(s string, n int) rune {
 	var requestedChar rune
-	if n < len(s) && n > 0 {
+	if n <= len(s) && n > 0 {
 		stringChars := []rune(s)
-		requestedChar = stringChars[n]
+		requestedChar = stringChars[n-1]
 	} else {
 		return 0
 	}
