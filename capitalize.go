28e95857b449d95c818b87f2d8490b9052fafaaf
srm <saadrm.dev@gmail.com>
2024-07-31 15:29:58 +0100

    Fix: capitalize

--- capitalize.go
+++ capitalize.go
@@ -1,54 +1,25 @@
 package piscine
 
-func ToUpper(s string) string {
-	stringUpperCase := ""
-
-	for i := range s {
-		if s[i] >= 'a' && s[i] <= 'z' {
-			stringUpperCase += string(s[i] - 32)
-		} else {
-			stringUpperCase += string(s[i])
-		}
-	}
-	return stringUpperCase
-}
-
-func IsAlpha(s string) bool {
-	if s == "" {
-		return true
-	}
-
-	for i := range s {
-		if s[i] < 'A' && s[i] > '9' {
-			return false
-		}
-		if s[i] < 'a' && s[i] > 'Z' {
-			return false
-		}
-		if s[i] < '0' || s[i] > 'z' {
-			return false
-		}
-	}
-
-	return true
-}
-
 func Capitalize(s string) string {
 	shouldCapitalize := true
-	newString := ""
+	result := []rune(s)
 
-	for i := range s {
+	for i := 0; i < len(result); i++ {
 		if IsAlpha(string(s[i])) {
 			if shouldCapitalize {
-				newString += ToUpper(string(s[i]))
+				if IsLower(string(s[i])) {
+					result[i] = rune(s[i] - 32)
+				} else {
+					result[i] = rune(s[i])
+				}
 			} else {
-				newString += string(s[i])
+				result[i] = rune(s[i])
 			}
 			shouldCapitalize = false
 		} else {
-			newString += string(s[i])
+			result[i] = rune(s[i])
 			shouldCapitalize = true
 		}
 	}
-	return newString
+	return string(result)
 }
