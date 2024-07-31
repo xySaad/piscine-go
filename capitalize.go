13c02d5c3f3255c706275bc4e485e9619fe54a1a
srm <saadrm.dev@gmail.com>
2024-07-31 15:36:30 +0100

    .

--- capitalize.go
+++ capitalize.go
@@ -1,34 +1,5 @@
 package piscine
 
-func IsAlpha(s string) bool {
-	if s == "" {
-		return true
-	}
-
-	for i := range s {
-		if s[i] < 'A' && s[i] > '9' {
-			return false
-		}
-		if s[i] < 'a' && s[i] > 'Z' {
-			return false
-		}
-		if s[i] < '0' || s[i] > 'z' {
-			return false
-		}
-	}
-
-	return true
-}
-
-func IsLower(s string) bool {
-	for i := range s {
-		if s[i] < 'a' || s[i] > 'z' {
-			return false
-		}
-	}
-	return true
-}
-
 func Capitalize(s string) string {
 	shouldCapitalize := true
 	result := []rune(s)
@@ -41,6 +12,8 @@ func Capitalize(s string) string {
 				} else {
 					result[i] = rune(s[i])
 				}
+			} else if IsUpper(string(s[i])) {
+				result[i] = rune(s[i] + 32)
 			} else {
 				result[i] = rune(s[i])
 			}
