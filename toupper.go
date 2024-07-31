2f55874ff1ff416cec93d1e8d3fc27517dc3bb8a
srm <saadrm.dev@gmail.com>
2024-07-31 11:21:03 +0100

    .

--- toupper.go
+++ toupper.go
@@ -6,7 +6,6 @@ func ToUpper(s string) string {
 	for i := range s {
 		if s[i] >= 'a' && s[i] <= 'z' {
 			stringUpperCase += string(s[i] - 32)
-
 		} else {
 			stringUpperCase += string(s[i])
 		}
