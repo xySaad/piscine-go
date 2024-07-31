f4aaed04b31b755ca75eeea4e390132f45209e0e
Christopher  Fremond <dev@01-edu.org>
2024-07-31 14:03:56 +0000

    Update 'capitalize.go'

--- capitalize.go
+++ capitalize.go
@@ -1,5 +1,38 @@
 package piscine
 
+func ToUpper(s string) string {
+	stringUpperCase := ""
+
+	for i := range s {
+		if s[i] >= 'a' && s[i] <= 'z' {
+			stringUpperCase += string(s[i] - 32)
+		} else {
+			stringUpperCase += string(s[i])
+		}
+	}
+	return stringUpperCase
+}
+
+func IsAlpha(s string) bool {
+	if s == "" {
+		return true
+	}
+
+	for i := range s {
+		if s[i] < 'A' && s[i] > '9' {
+			return false
+		}
+		if s[i] < 'a' && s[i] > 'Z' {
+			return false
+		}
+		if s[i] < '0' || s[i] > 'z' {
+			return false
+		}
+	}
+
+	return true
+}
+
 func Capitalize(s string) string {
 	shouldCapitalize := true
 	newString := ""
