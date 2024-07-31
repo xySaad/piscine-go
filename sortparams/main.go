e805005c4a24d0aaae681cd053b6a708a8eea88b
srm <saadrm.dev@gmail.com>
2024-07-31 21:55:51 +0100

    sortparams

---
+++ sortparams/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func sortRunes(table []string) {
+	for i := 0; i < len(table)-1; i++ {
+		for i := 0; i < len(table)-1; i++ {
+			prev := table[i]
+			next := table[i+1]
+			if prev > next {
+				table[i] = next
+				table[i+1] = prev
+			}
+		}
+	}
+}
+func main() {
+	if len(os.Args) > 0 {
+		sortRunes(os.Args)
+		for argsIndex := 1; argsIndex < len(os.Args); argsIndex++ {
+			argTable := []rune(os.Args[argsIndex])
+			for index := range argTable {
+				z01.PrintRune(argTable[index])
+			}
+			z01.PrintRune('\n')
+		}
+	}
+}
