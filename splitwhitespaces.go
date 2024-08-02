4de793a5234a9a9d926593624ffa909dc040238f
srm <saadrm.dev@gmail.com>
2024-08-02 12:24:56 +0100

    .

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -4,10 +4,15 @@ func SplitWhiteSpaces(s string) []string {
 	runes := []rune(s)
 	result := []string{}
 	index := 0
+	inWord := true
 	for i := 0; i < len(runes); i++ {
 		if runes[i] == '\n' || runes[i] == ' ' || runes[i] == '\t' {
-			index++
+			if inWord {
+				index++
+			}
+			inWord = false
 		} else {
+			inWord = true
 			if len(result) > index {
 				result[index] += string(runes[i])
 			} else {
