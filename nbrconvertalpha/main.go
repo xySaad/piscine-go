493bab6da3c2705b14ae2a7e3fc84ef9281424b8
srm <saadrm.dev@gmail.com>
2024-08-02 09:44:59 +0100

    Fix : illegal-import (piscine)

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -2,10 +2,33 @@ package main
 
 import (
 	"os"
-	"piscine"
 
 	"github.com/01-edu/z01"
 )
+
+func Atoi(s string) int {
+	var numberAsInteger int = 0
+	var sign int = 1
+	for i := range s {
+		var num int = 0
+		if s[i] >= '0' && s[i] <= '9' {
+			num = int(s[i] - '0')
+		} else if i == 0 {
+			switch s[i] {
+			case '-':
+				sign = -1
+			case '+':
+				sign = 1
+			default:
+				return 0
+			}
+		} else {
+			return 0
+		}
+		numberAsInteger = numberAsInteger*10 + num
+	}
+	return numberAsInteger * sign
+}
 
 func main() {
 	args := os.Args[1:]
@@ -17,7 +40,7 @@ func main() {
 	}
 
 	for i := range args {
-		num := piscine.Atoi(args[i])
+		num := Atoi(args[i])
 		if num >= 1 && num <= 25 {
 			if isUpper {
 				z01.PrintRune(rune('A' + num - 1))
