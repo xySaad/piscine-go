53c3b570a55426a40e951685ff65a835eef6e93b
srm <saadrm.dev@gmail.com>
2024-08-05 14:37:38 +0100

    boolean

---
+++ point/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "github.com/01-edu/z01"
+
+type point struct {
+	x int
+	y int
+}
+
+func numToString(num int) string {
+	var result string = ""
+
+	for num > 0 {
+		result = string(rune(num%10+'0')) + result
+		num /= 10
+	}
+	return result
+}
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
+}
+
+func main() {
+
+	points := point{}
+
+	setPoint(&points)
+
+	printStr("x = " + numToString(points.x) + ", y = " + numToString(points.y))
+}
