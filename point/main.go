fa455eea8f96c920704710751e6a7104e7ea3842
srm <saadrm.dev@gmail.com>
2024-08-05 15:04:57 +0100

    .

--- point/main.go
+++ point/main.go
@@ -7,32 +7,29 @@ type point struct {
 	y int
 }
 
-func printStr(s string) {
-	for _, r := range s {
-		z01.PrintRune(r)
-	}
-	z01.PrintRune('\n')
-}
-
 func setPoint(ptr *point) {
 	ptr.x = 42
 	ptr.y = 21
 }
 
 func main() {
-	points := point{}
+	points := &point{}
 
-	setPoint(&points)
+	setPoint(points)
 
-	numToString := func(num int) string {
-		var result string = ""
-
-		for num > 0 {
-			result = string(rune(num%10+'0')) + result
-			num /= 10
-		}
-		return result
-	}
-
-	printStr("x = " + numToString(points.x) + ", y = " + numToString(points.y))
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	z01.PrintRune('0' + int32(points.x/10))
+	z01.PrintRune('0' + int32(points.x%10))
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	z01.PrintRune('0' + int32(points.y/10))
+	z01.PrintRune('0' + int32(points.y%10))
+	z01.PrintRune('\n')
 }
