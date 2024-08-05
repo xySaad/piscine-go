4cd953aee1ee31559d45c27981ffeedaf32e6b5e
srm <saadrm.dev@gmail.com>
2024-08-05 16:51:46 +0100

    displayfile

---
+++ displayfile/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		println("File name missing")
+	} else if len(os.Args) > 2 {
+		println("Too many arguments")
+	} else {
+		fileYmkn, err := os.ReadFile(os.Args[1])
+
+		if err != nil {
+			return
+		}
+
+		fmt.Println(string(fileYmkn))
+	}
+}
