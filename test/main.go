a844eed53c864b0a5a068e712bf788da2d90ee2a
srm <saadrm.dev@gmail.com>
2024-08-06 16:38:03 +0100

    any

--- test/main.go
+++ test/main.go
@@ -6,7 +6,12 @@ import (
 )
 
 func main() {
-	a := []int{1, 2, 3, 4, 5, 6}
-	result := piscine.Map(piscine.IsPrime, a)
-	fmt.Println(result)
+	a1 := []string{"Hello", "how", "are", "you"}
+	a2 := []string{"This", "is", "4", "you"}
+
+	result1 := piscine.Any(piscine.IsNumeric, a1)
+	result2 := piscine.Any(piscine.IsNumeric, a2)
+
+	fmt.Println(result1)
+	fmt.Println(result2)
 }
