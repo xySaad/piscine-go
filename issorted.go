6cac945c00eecf7a5cb15c62bcd1365d1d626b0a
srm <saadrm.dev@gmail.com>
2024-08-07 05:10:39 +0100

    handle sortedbyDiff case

--- issorted.go
+++ issorted.go
@@ -2,6 +2,10 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 0; i < len(a)-1; i++ {
+		if f(a[i], a[i+1]) == a[i]-a[i+1] && f(a[i], a[i+1]) < 0 {
+			return false
+		}
+
 		if f(a[i], a[i+1]) > 0 {
 			return false
 		}
