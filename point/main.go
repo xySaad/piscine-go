15314af9e30102d25d08557cf3cd9fed59ffd01c
srm <saadrm.dev@gmail.com>
2024-08-07 12:00:27 +0100

    point

--- point/main.go
+++ point/main.go
@@ -1,25 +1,51 @@
 package main
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 type point struct {
 	x int
 	y int
 }
 
-func numToString(num int) string {
-	var result string = ""
-	for num > 0 {
-		var temp interface{} = num%10 + '0'
-		result = temp.(string) + result
-		num /= 10
+func intSize(n int) int {
+	size := 1
+
+	for n >= 10 {
+		n /= 10
+		size *= 10
 	}
-	return result
+	return size
 }
 
-func printStr(s string) {
-	for _, r := range s {
-		z01.PrintRune(r)
+func intToRunes(n int) (result rune) {
+	for i := 0; i < 32; i++ {
+		if n&(1<<i) != 0 {
+			result |= 1 << i
+		}
+	}
+	return
+}
+
+func printStr(table ...interface{}) {
+
+	for _, r := range table {
+		switch VType := r.(type) {
+		case string:
+			for _, v := range VType {
+				z01.PrintRune(v)
+			}
+		case int:
+			num := VType
+			size := intSize(num)
+			for size > 0 {
+				z01.PrintRune(intToRunes(num/size%10 + '0'))
+				size /= 10
+			}
+		default:
+			printStr("Invalid Type", VType)
+		}
 	}
 	z01.PrintRune('\n')
 }
@@ -32,5 +58,6 @@ func setPoint(ptr *point) {
 func main() {
 	points := point{}
 	setPoint(&points)
-	printStr("x = " + numToString(points.x) + ", y = " + numToString(points.y))
+
+	printStr("x = ", points.x, ", y = ", points.y)
 }
