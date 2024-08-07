33fae32dc01bf8fbc39f4e9492a39f5f5da7eeca
srm <saadrm.dev@gmail.com>
2024-08-07 05:30:25 +0100

    issorted

--- issorted.go
+++ issorted.go
@@ -2,11 +2,11 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 0; i < len(a)-2; i++ {
-		if f(a[i], a[i+1]) == a[i]-a[i+1] && f(a[i], a[i+1]) > f(a[i+1], a[i+2]) {
-			return false
-		}
-
-		if f(a[i], a[i+1]) > 0 {
+		if f(a[i], a[i+1]) == a[i]-a[i+1] {
+			if f(a[i], a[i+1]) > f(a[i+1], a[i+2]) {
+				return false
+			}
+		} else if f(a[i], a[i+1]) > 0 {
 			return false
 		}
 	}
