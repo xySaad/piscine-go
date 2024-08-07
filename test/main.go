cefef7361b85055d5402583f2b7d1504cedb904f
srm <saadrm.dev@gmail.com>
2024-08-07 13:08:01 +0100

    collatzcountdown

--- test/main.go
+++ test/main.go
@@ -6,8 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.FoodDeliveryTime("burger"))
-	fmt.Println(piscine.FoodDeliveryTime("chips"))
-	fmt.Println(piscine.FoodDeliveryTime("nuggets"))
-	fmt.Println(piscine.FoodDeliveryTime("burger") + piscine.FoodDeliveryTime("chips") + piscine.FoodDeliveryTime("nuggets"))
+	steps := piscine.CollatzCountdown(12)
+	fmt.Println(steps)
 }
